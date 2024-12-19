31bec163e0be9b5d67c57c39b3ad6c81d0303d6a
Scott Kirvan <61367618+ScottKirvan@users.noreply.github.com>
2024-12-19 12:01:10 -0800

    fix: do it again

--- runsed.sh
+++ runsed.sh
@@ -7,11 +7,29 @@ pwd
 VERSION="1.2.13"
 echo "Version: $VERSION"
 
-# Update the uplugin file
-echo "Updating ScooterUtils.uplugin"
-sed -i "s/\"VersionName\": \"v[0-9]*\.[0-9]*\.[0-9]*\"/\"VersionName\": \"v$VERSION\"/" ScooterUtils.uplugin
+PATCH=17
+echo "Major: $PATCH"
+
+
+
+echo "Updating Source/ScooterUtils/Public/ScooterUtilsVersion.h"
+sed -i 's/#define VERSION_PATCH [0-9]*/#define VERSION_PATCH '"$PATCH"'/' Source/ScooterUtils/Public/ScooterUtilsVersion.h
 if [ $? -eq 0 ]; then echo "uplugin VersionName updated successfully"; else echo "Failed to update uplugin VersionName"; fi
 
-# Print the contents of the uplugin file after updating
-echo "Contents of ScooterUtils.uplugin after update:"
-cat ./ScooterUtils.uplugin
+# Print the contents of the header file after updating
+echo "Contents of Source/ScooterUtils/Public/ScooterUtilsVersion.h after update:"
+cat ./Source/ScooterUtils/Public/ScooterUtilsVersion.h
+
+# Update the uplugin file
+#echo "Updating ScooterUtils.uplugin"
+#sed -i "s/\"VersionName\": \"v[0-9]*\.[0-9]*\.[0-9]*\"/\"VersionName\": \"v$VERSION\"/" ScooterUtils.uplugin
+#if [ $? -eq 0 ]; then echo "uplugin VersionName updated successfully"; else echo "Failed to update uplugin VersionName"; fi
+
+## Print the contents of the uplugin file after updating
+#echo "Contents of ScooterUtils.uplugin after update:"
+#cat ./ScooterUtils.uplugin
+
+
+echo
+
+
